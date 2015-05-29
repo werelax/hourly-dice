6d957282a38e872d253c8dab809309d3d5600fbf
Elias A. G.-C <eliasagc@gmail.com>
2015-05-29 20:19:00 +0200

    starting commit

---
+++ env/prod/cljs/dice/prod.cljs
@@ -0,0 +1,7 @@
+(ns dice.prod
+  (:require [dice.core :as core]))
+
+;;ignore println statements in prod
+(set! *print-fn* (fn [& _]))
+
+(core/init!)
---
+++ src/clj/dice/handler.clj
@@ -0,0 +1,29 @@
+(ns dice.handler
+  (:require [compojure.core :refer [GET defroutes]]
+            [compojure.route :refer [not-found resources]]
+            [ring.middleware.defaults :refer [site-defaults wrap-defaults]]
+            [hiccup.core :refer [html]]
+            [hiccup.page :refer [include-js include-css]]
+            [prone.middleware :refer [wrap-exceptions]]
+            [environ.core :refer [env]]))
+
+(def home-page
+  (html
+   [:html
+    [:head
+     [:meta {:charset "utf-8"}]
+     [:meta {:name "viewport"
+             :content "width=device-width, initial-scale=1"}]
+     (include-css (if (env :dev) "css/site.css" "css/site.min.css"))]
+    [:body
+     [:div#app]
+     (include-js "js/app.js")]]))
+
+(defroutes routes
+  (GET "/" [] home-page)
+  (resources "/")
+  (not-found "Not Found"))
+
+(def app
+  (let [handler (wrap-defaults routes site-defaults)]
+    (if (env :dev) (wrap-exceptions handler) handler)))
